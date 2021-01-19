52f10be329629157c548347c04437752de092d98
junhongGenesislab <unique3801@gmail.com>
2021-01-19 09:16:46 +0900

    basic login

--- src/models.rs
+++ src/models.rs
@@ -9,3 +9,12 @@ pub struct User {
   pub email: String, 
   pub created_at: chrono::NaiveDateTime, 
 }
+
+#[derive(Insertable, Debug)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+  pub first_name: &'a str, 
+  pub last_name: &'a str, 
+  pub email: &'a str, 
+  pub created_at: chrono::NaiveDateTime,
+}
